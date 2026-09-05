c44c8cabb2cc6b16a7a9a69c3a28743c37158fa7
duckie <duckiegolucky@gmail.com>
2026-09-05 16:42:12 +0800

    Delete 911.com directory

--- 911.com/functions/api/list.js
+++
@@ -1,7 +0,0 @@
-import { json, readIndex } from '../_lib.js';
-
-export async function onRequestGet({ env }) {
-  const list = await readIndex(env);
-  list.sort((a, b) => (a.order - b.order) || (a.addedAt - b.addedAt));
-  return json({ tracks: list });
-}
--- 911.com/functions/api/logout.js
+++
@@ -1,5 +0,0 @@
-import { json, SESSION } from '../_lib.js';
-
-export const onRequestPost = () => json({ ok: true }, 200, {
-  'set-cookie': `${SESSION}=; Path=/; HttpOnly; Secure; SameSite=Lax; Max-Age=0`
-});
